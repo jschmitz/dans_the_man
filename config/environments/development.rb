5562c560d18b17dd3b050d07fb49105eb7a5e9c6
Jake Schmitz <jschmitz@gmail.com>
2013-07-07 13:29:26 -0500

    View Updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,11 +27,4 @@ DansTheMan::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-
-  # Set the logging destination(s)
-  config.log_to = %w[stdout file]
-
-  # Show the logging configuration on STDOUT
-  config.show_log_configuration = true
 end
